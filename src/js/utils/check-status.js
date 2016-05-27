28a1cc25c48a7df75c01af2e2214cdad2a249b5f
tsuyoshiwada <v23.b.wada@gmail.com>
2016-05-28 02:44:03 +0900

    Add fetch utility

---
+++ src/js/utils/check-status.js
@@ -0,0 +1,11 @@
+"use strict";
+
+export default function checkStatus(res) {
+  if (res.status >= 200 && res.status < 300) {
+    return res;
+  } else {
+    const error = new Error(res.statusText);
+    error.response = res;
+    throw error;
+  }
+}
