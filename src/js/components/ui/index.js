53600fba5a218e227f66d8d9d46e1db3aa48f720
tsuyoshiwada <v23.b.wada@gmail.com>
2016-06-10 02:23:21 +0900

    Add ui component index

---
+++ src/js/components/ui/index.js
@@ -0,0 +1,5 @@
+export Dialog from "./Dialog/Dialog";
+export FlatButton from "./FlatButton/FlatButton";
+export FloatingButton from "./FloatingButton/FloatingButton";
+export IconButton from "./IconButton/IconButton";
+export RaisedButton from "./RaisedButton/RaisedButton";
