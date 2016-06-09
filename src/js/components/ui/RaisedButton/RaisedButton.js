75244d41695369b47a555f160db9af8e9b06bf0c
tsuyoshiwada <v23.b.wada@gmail.com>
2016-06-09 15:19:05 +0900

    Add Button component and refactor RaisedButton component

--- src/js/components/ui/RaisedButton/RaisedButton.js
+++ src/js/components/ui/RaisedButton/RaisedButton.js
@@ -1,15 +1,10 @@
 import React, { PropTypes } from "react";
-import Ripple from "../internal/Ripple";
-import bem from "../../../helpers/bem";
-import bindHandlers from "../../../helpers/bind-handlers";
-import randomId from "../../../helpers/random-id";
-
-const b = bem("raised-button");
+import Button from "../internal/Button";
 
 export default class RaisedButton extends React.Component {
   static propTypes = {
     type: PropTypes.string.isRequired,
-    children: PropTypes.element,
+    children: PropTypes.node,
     href: PropTypes.string,
     target: PropTypes.string,
     icon: PropTypes.element,
@@ -18,101 +13,19 @@ export default class RaisedButton extends React.Component {
   };
 
   static defaultProps = {
-    type: "default",
-    onClick: () => {}
+    type: "default"
   };
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isAnimate: true,
-      ripples: []
-    };
-
-    bindHandlers([
-      "handleClick",
-      "handleRippleHide",
-      "handleMouseDown"
-    ], this);
-  }
-
-  getRippleStyle(x, y, size) {
-    return {
-      top: y - size / 2,
-      left: x - size / 2,
-      width: size,
-      height: size
-    };
-  }
-
-  addRippleElement(x, y, size) {
-    const style = this.getRippleStyle(x, y, size);
-
-    this.setState({
-      ripples: this.state.ripples.concat([
-        <Ripple
-          key={randomId()}
-          className={b("ripple", { [this.props.type]: true })}
-          style={style}
-          onRequestHide={this.handleRippleHide}
-        />
-      ])
-    });
-  }
-
-  createIcon(icon, className) {
-    return icon ? <span className={className}>{icon}</span> : null;
-  }
-
-  handleMouseDown(e) {
-    const { top, left, width, height } = this.refs.element.getBoundingClientRect();
-
-    this.addRippleElement(
-      e.pageX - (left + window.pageXOffset),
-      e.pageY - (top + window.pageYOffset),
-      Math.max(width, height) * 1.5
-    );
-  }
-
-  handleRippleHide() {
-    this.setState({ ripples: this.state.ripples.slice(1) });
-  }
-
-  handleClick(e) {
-    if (this.props.onClick(e)) {
-      this.props.onClick(e);
-    }
-  }
 
   render() {
     const {
       children,
-      type,
-      href,
-      target
+      ...props
     } = this.props;
 
-    const { ripples } = this.state;
-    const modifier = { [type]: true };
-    const label = <span className={b("label", modifier)} ref="label">{children}</span>;
-    const icon = this.createIcon(this.props.icon, b("icon"));
-    const iconRight = this.createIcon(this.props.iconRight, b("icon", { right: true }));
-    const bodyClass = b("body", modifier);
-    const body = !href
-      ? <button className={bodyClass} type="button" ref="body">{icon}{label}{iconRight}</button>
-      : <a className={bodyClass} href={href} target={target} ref="body">{icon}{label}{iconRight}</a>;
-
-    return (
-      <div
-        className={b(modifier)}
-        ref="element"
-        onMouseDown={this.handleMouseDown}
-        onClick={this.handleClick}
-      >
-        <div className={b("ripple-container")}>{ripples}</div>
-        {body}
-      </div>
-    );
+    return <Button
+      className="raised-button"
+      label={children}
+      {...props}
+      />;
   }
 }
---
+++ src/js/components/ui/internal/Button.js
@@ -0,0 +1,122 @@
+import React, { PropTypes } from "react";
+import Ripple from "./Ripple";
+import bem from "../../../helpers/bem";
+import bindHandlers from "../../../helpers/bind-handlers";
+import randomId from "../../../helpers/random-id";
+
+export default class Button extends React.Component {
+  static propTypes = {
+    className: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    href: PropTypes.string,
+    target: PropTypes.string,
+    label: PropTypes.node,
+    icon: PropTypes.element,
+    iconRight: PropTypes.element,
+    onClick: PropTypes.func
+  };
+
+  static defaultProps = {
+    type: "default",
+    onClick: () => {}
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ripples: []
+    };
+
+    bindHandlers([
+      "handleClick",
+      "handleMouseDown",
+      "handleRippleHide"
+    ], this);
+  }
+
+  getRippleStyle(x, y, size) {
+    return {
+      top: y - size / 2,
+      left: x - size / 2,
+      width: size,
+      height: size
+    };
+  }
+
+  addRippleElement(x, y, size) {
+    const { className, type } = this.props;
+    const { ripples } = this.state;
+    const b = bem(className);
+    const style = this.getRippleStyle(x, y, size);
+
+    this.setState({
+      ripples: ripples.concat([
+        <Ripple
+          key={randomId()}
+          className={b("ripple", { [type]: true })}
+          style={style}
+          onRequestHide={this.handleRippleHide}
+        />
+      ])
+    });
+  }
+
+  createIcon(icon, className) {
+    return icon ? <span className={className}>{icon}</span> : null;
+  }
+
+  handleClick(e) {
+    this.props.onClick(e);
+  }
+
+  handleMouseDown(e) {
+    const { top, left, width, height } = this.refs.element.getBoundingClientRect();
+
+    this.addRippleElement(
+      e.pageX - (left + window.pageXOffset),
+      e.pageY - (top + window.pageYOffset),
+      Math.max(width, height) * 1.5
+    );
+  }
+
+  handleRippleHide() {
+    const ripples = this.state.ripples.slice(1);
+    this.setState({ ripples });
+  }
+
+  render() {
+    const {
+      className,
+      type,
+      href,
+      target,
+      label,
+      icon,
+      iconRight
+    } = this.props;
+
+    const { ripples } = this.state;
+    const b = bem(className);
+    const modifier = { [type]: true };
+    const labelElement = label ? <span className={b("label", modifier)}>{label}</span> : null;
+    const iconElement = this.createIcon(icon, b("icon"));
+    const iconRightElement = this.createIcon(iconRight, b("icon", { right: true }));
+    const bodyClass = b("body", modifier);
+    const bodyElement = !href
+      ? <button className={bodyClass}>{iconElement}{labelElement}{iconRightElement}</button>
+      : <a className={bodyClass} href={href} target={target}>{iconElement}{labelElement}{iconRightElement}</a>;
+
+    return (
+      <div
+        className={b(modifier)}
+        ref="element"
+        onMouseDown={this.handleMouseDown}
+        onClick={this.handleClick}
+      >
+        <div className={b("ripple-container")}>{ripples}</div>
+        {bodyElement}
+      </div>
+    );
+  }
+}
