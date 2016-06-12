e0c7964e83bb14719f8c083e8233cbd19d83979b
tsuyoshiwada <v23.b.wada@gmail.com>
2016-06-12 23:06:11 +0900

    Add component index

--- src/js/components/ui/index.js
+++ src/js/components/ui/index.js
@@ -3,3 +3,5 @@ export FlatButton from "./FlatButton/FlatButton";
 export FloatingButton from "./FloatingButton/FloatingButton";
 export IconButton from "./IconButton/IconButton";
 export RaisedButton from "./RaisedButton/RaisedButton";
+export Slider from "./Slider/Slider";
+export TextField from "./TextField/TextField";
