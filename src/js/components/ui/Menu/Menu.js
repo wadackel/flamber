73ee233ac5e57be9c77d239ed934fd88e7bf0fa7
tsuyoshiwada <v23.b.wada@gmail.com>
2016-06-13 00:44:55 +0900

    Add Menu component

---
+++ src/js/components/ui/Menu/Menu.js
@@ -0,0 +1,32 @@
+import React, { PropTypes } from "react";
+import bem from "../../../helpers/bem";
+import bindHandlers from "../../../helpers/bind-handlers";
+import MenuItem from "./MenuItem";
+
+const b = bem("menu");
+
+export default class Menu extends React.Component {
+  static propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.node,
+    open: PropTypes.bool
+  };
+
+  static defaultProps = {
+    open: false
+  };
+
+  render() {
+    const {
+      className,
+      children,
+      open
+    } = this.props;
+
+    return (
+      <div className={`${b()} ${className ? className : ""}`}>
+        {children}
+      </div>
+    );
+  }
+}
---
+++ src/js/components/ui/Menu/README.md
@@ -0,0 +1,9 @@
+Basic:
+
+```
+<Menu open={true}>
+  <MenuItem text="Item 1" />
+  <MenuItem text="Item 2" />
+  <MenuItem text="Item 3" />
+</Menu>
+```
