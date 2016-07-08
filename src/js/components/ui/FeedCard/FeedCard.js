4245bc30eaa4d9358dd108181363699be424a05c
tsuyoshiwada <v23.b.wada@gmail.com>
2016-07-09 02:52:29 +0900

    Add FeedCard list styles

--- src/js/components/ui/FeedCard/FeedCard.js
+++ src/js/components/ui/FeedCard/FeedCard.js
@@ -1,12 +1,15 @@
 import urlParse from "url-parse";
 import React, { PropTypes } from "react";
+import * as Layout from "../../../constants/layouts";
 import bem from "../../../helpers/bem";
 import mergeClassNames from "../../../helpers/merge-class-names";
 import bindHandlers from "../../../helpers/bind-handlers";
 import {
   Card,
   CardBody,
+  CardCol,
   CardMedia,
+  CardMore,
   CardOverlay,
   CardText,
   CardTitle
@@ -19,6 +22,7 @@ export default class FeedCard extends React.Component {
   static propTypes = {
     className: PropTypes.string,
     style: PropTypes.object,
+    layout: PropTypes.oneOf([Layout.GRID, Layout.LIST]),
     url: PropTypes.string,
     site: PropTypes.string,
     favicon: PropTypes.string,
@@ -30,6 +34,7 @@ export default class FeedCard extends React.Component {
   };
 
   static defaultProps = {
+    layout: Layout.GRID,
     style: {},
     onAddItem: () => {}
   };
@@ -46,7 +51,62 @@ export default class FeedCard extends React.Component {
     // TODO
   }
 
-  render() {
+  renderMoreActions() {
+    return [
+      <FlatButton onClick={this.handleDetailClick}>Visit</FlatButton>,
+      <FlatButton onClick={this.handleAddItem}>Add item</FlatButton>
+    ];
+  }
+
+  renderList() {
+    const {
+      className,
+      style,
+      url,
+      site,
+      favicon,
+      title,
+      image
+    } = this.props;
+
+    const baseClassName = "feed-card--list";
+    const b = bem(baseClassName);
+    const parsedURL = urlParse(site, true);
+
+    return (
+      <Card
+        baseClassName={mergeClassNames(b(), className)}
+        style={style}
+        onMouseLeave={this.handleMouseLeave}
+      >
+        <CardCol baseClassName={baseClassName} className={b("col--media")}>
+          <CardMedia
+            baseClassName={baseClassName}
+            image={image}
+          />
+        </CardCol>
+        <CardCol baseClassName={baseClassName} className={b("col--body")}>
+          <CardBody baseClassName={baseClassName}>
+            <CardTitle baseClassName={baseClassName}>{title}</CardTitle>
+            <CardText baseClassName={baseClassName}>
+              <a href={url} target="_blank">
+                <img className={b("favicon")} src={favicon} alt={parsedURL.host} />
+                {parsedURL.host}
+              </a>
+            </CardText>
+          </CardBody>
+        </CardCol>
+        <CardCol baseClassName={baseClassName} className={b("col--more")}>
+          <CardMore
+            baseClassName={baseClassName}
+            actions={this.renderMoreActions()}
+          />
+        </CardCol>
+      </Card>
+    );
+  }
+
+  renderGrid() {
     const {
       className,
       style,
@@ -94,5 +154,16 @@ export default class FeedCard extends React.Component {
         </CardBody>
       </Card>
     );
+  }
+
+  render() {
+    const { layout } = this.props;
+
+    switch (layout) {
+      case Layout.GRID:
+        return this.renderGrid();
+      case Layout.LIST:
+        return this.renderList();
+    }
   }
 }
--- src/js/components/ui/FeedCard/README.md
+++ src/js/components/ui/FeedCard/README.md
@@ -24,3 +24,32 @@ class FeedCardExample extends React.Component {
 
 <FeedCardExample />
 ```
+
+
+List:
+
+```
+class FeedCardListExample extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <FeedCard
+        layout="list"
+        style={{ width: 270 }}
+        url="http://example.com/path/to/"
+        site="https://blog.wadackel.me/"
+        favicon="/images/sample-favicon.ico"
+        title="Dripup Creative Design Pallete"
+        image="/images/feed-card-sample1.png"
+        imageWidth={540}
+        imageHeight={798}
+      />
+    );
+  }
+}
+
+<FeedCardListExample />
+```
