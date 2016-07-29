dfe5956cf359d25e19688444fbc7b779f5eae6b6
tsuyoshiwada <v23.b.wada@gmail.com>
2016-07-29 19:27:07 +0900

    Add ToolBox component

--- src/js/components/ui/ToolBox/README.md
+++ src/js/components/ui/ToolBox/README.md
@@ -10,7 +10,15 @@ class ToolBoxExample extends React.Component {
   render() {
     return (
       <div>
-        <RaisedButton>Toggle</RaisedButton>
+        <RaisedButton onClick={() => this.setState({ open: !this.state.open })}>Toggle</RaisedButton>
+        <ToolBox
+          open={this.state.open}
+          text="Tool box text"
+          actions={[
+            <IconButton type="primary" tooltip="Move item" icon={<FolderIcon />} />,
+            <IconButton type="primary" tooltip="Delete item" icon={<TrashIcon />} />
+          ]}
+        />
       </div>
     );
   }
--- src/js/components/ui/ToolBox/ToolBox.js
+++ src/js/components/ui/ToolBox/ToolBox.js
@@ -1,30 +1,78 @@
-/* eslint-disable */
 import React, { Component, PropTypes } from "react";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import shareConfig from "../../../../share-config.json";
 import bem from "../../../helpers/bem";
 import mergeClassNames from "../../../helpers/merge-class-names";
 import bindHandlers from "../../../helpers/bind-handlers";
+import FirstChild from "../internal/FirstChild";
+import RenderToLayer from "../internal/RenderToLayer";
 
-const b = bem("toolbox");
+const b = bem("tool-box");
 
 export default class ToolBox extends Component {
   static propTypes = {
+    className: PropTypes.string,
+    open: PropTypes.bool,
+    text: PropTypes.node,
+    actions: PropTypes.array
   };
 
   static defaultProps = {
+    open: false,
+    text: "",
+    actions: []
   };
 
   constructor(props, context) {
     super(props, context);
 
-    this.state = {};
-
     bindHandlers([
+      "renderLayer"
     ], this);
   }
 
-  render() {
+  renderBox() {
+    const { className, open, text, actions } = this.props;
+    const modifier = { open };
+
+    if (!open) return null;
+
     return (
-      <div>TODO</div>
+      <div className={mergeClassNames(b(modifier), className)}>
+        <div className={b("inner", modifier)}>
+          <div className={b("body", modifier)}>
+            <div className={b("text", modifier)}>{text}</div>
+            <ul className={b("actions", modifier)}>
+              {actions.map((action, index) =>
+                <li key={index} className={b("action")}>{action}</li>
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
     );
+  }
+
+  renderLayer() {
+    return (
+      <div>
+        <ReactCSSTransitionGroup
+          component={FirstChild}
+          transitionName="open"
+          transitionEnterTimeout={shareConfig["tool-box-enter-duration"]}
+          transitionLeaveTimeout={shareConfig["tool-box-leave-duration"]}
+        >
+          {this.renderBox()}
+        </ReactCSSTransitionGroup>
+      </div>
+    );
+  }
+
+  render() {
+    return <RenderToLayer
+      render={this.renderLayer}
+      open={true}
+      useLayerForClickAway={false}
+    />;
   }
 }
