e44b3f00b9a695016a2d1070c8346fb5c1bc0bcc
tsuyoshiwada <v23.b.wada@gmail.com>
2016-08-20 00:13:55 +0900

    Add DropIcon

---
+++ src/js/components/svg-icons/DropIcon.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function DropIcon(props) {
+  return (
+    <svg width="20" height="30" viewBox="0 0 20 30" {...props}><title>drop</title><path d="M10 0C3.912 8.996 0 14.624 0 20.094 0 25.566 4.474 30 10 30s10-4.434 10-9.906C20 14.624 16.087 8.996 10 0zm.578 25.59c3.2-1.313 5.155-6.113 3.776-11.067 4.747 6.136.942 11.74-3.776 11.066z" /></svg>
+  );
+}
