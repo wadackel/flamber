d8d14aac8043c75a02cd197272b43de66fb24f69
tsuyoshiwada <v23.b.wada@gmail.com>
2016-09-05 15:36:48 +0900

    Add favicon

--- src/js/server.js
+++ src/js/server.js
@@ -44,6 +44,8 @@ function HTML({ content, store }) {
         {head.title.toComponent()}
         {head.style.toComponent()}
         <link rel="stylesheet" href="/css/style.css" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
       <body>
         <div id="app" className="app" dangerouslySetInnerHTML={{ __html: content }} />
