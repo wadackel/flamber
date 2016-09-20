b80e2cc348b88a69af66e380269cd00e928cfb5e
tsuyoshiwada <v23.b.wada@gmail.com>
2016-09-21 00:34:08 +0900

    Fix react-router warning

--- src/js/server.js
+++ src/js/server.js
@@ -105,26 +105,22 @@ app.use((req, res) => {
   const history = syncHistoryWithStore(memoryHistory, store);
 
   match({ history, routes: getRoutes(store), location: req.url }, (error, redirectLocation, renderProps) => {
-    let props = renderProps;
-
     if (redirectLocation) {
       res.redirect(302, redirectLocation.pathname + redirectLocation.search);
       return;
 
     } else if (error) {
-      res.status(500);
+      res.status(500).send(error.message);
+      return;
 
-    } else if (!renderProps) {
-      props = {};
-      res.status(404);
-
-    } else {
-      res.status(200);
+    } else if (renderProps == null) {
+      res.status(404).send("Not found");
+      return;
     }
 
     const content = renderToString(
       <Provider store={store}>
-        <RouterContext {...props} />
+        <RouterContext {...renderProps} />
       </Provider>
     );
 
