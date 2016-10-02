3ee47895db5f1513b0c52f9f325d07c959815e7d
tsuyoshiwada <v23.b.wada@gmail.com>
2016-10-02 21:31:30 +0900

    Add type definition of Progress components

--- src/js/components/ui/ProcessingOverlay/ProcessingOverlay.js
+++ src/js/components/ui/ProcessingOverlay/ProcessingOverlay.js
@@ -1,44 +1,43 @@
-import React, { Component, PropTypes } from "react";
+// @flow
+import React from "react";
 import bem from "../../../helpers/bem";
 import mergeClassNames from "../../../helpers/merge-class-names";
 import { Spinner } from "../";
 
 const b = bem("processing-overlay");
 
-export default class ProcessingOverlay extends Component {
-  static propTypes = {
-    className: PropTypes.string,
-    style: PropTypes.object,
-    show: PropTypes.bool,
-    spinnerSize: PropTypes.number
-  };
+type Props = {
+  className?: string;
+  style?: Object;
+  show: boolean;
+  spinnerSize: number;
+};
 
-  static defaultProps = {
-    style: {},
-    show: false,
-    spinnerSize: 28
-  };
+export default function ProcessingOverlay(props: Props) {
+  const {
+    className,
+    style,
+    show,
+    spinnerSize
+  } = props;
 
-  render() {
-    const {
-      className,
-      style,
-      show,
-      spinnerSize
-    } = this.props;
+  const modifier = { show };
 
-    const modifier = { show };
-
-    return (
-      <div className={mergeClassNames(b(modifier)(), className)} style={style}>
-        {show &&
-          <Spinner
-            className={b("spinner", modifier)()}
-            size={spinnerSize}
-            style={{ position: "absolute" }}
-          />
-        }
-      </div>
-    );
-  }
+  return (
+    <div className={mergeClassNames(b(modifier)(), className)} style={style}>
+      {show &&
+        <Spinner
+          className={b("spinner", modifier)()}
+          size={spinnerSize}
+          style={{ position: "absolute" }}
+        />
+      }
+    </div>
+  );
 }
+
+ProcessingOverlay.defaultProps = {
+  style: {},
+  show: false,
+  spinnerSize: 28
+};
--- src/js/components/ui/ProgressBar/ProgressBar.js
+++ src/js/components/ui/ProgressBar/ProgressBar.js
@@ -1,38 +1,37 @@
-import React, { PropTypes } from "react";
+// @flow
+import React from "react";
 import bem from "../../../helpers/bem";
 import mergeClassNames from "../../../helpers/merge-class-names";
 
 const b = bem("progress-bar");
 
-export default class ProgressBar extends React.Component {
-  static propTypes = {
-    className: PropTypes.string,
-    value: PropTypes.number.isRequired,
-    max: PropTypes.number,
-    min: PropTypes.number
+type Props = {
+  className?: string;
+  value: number;
+  max: number;
+  min: number;
+};
+
+export default function ProgressBar(props: Props) {
+  const {
+    className,
+    value,
+    max,
+    min
+  } = props;
+
+  const valueStyle = {
+    width: `${Math.min(Math.max(min, value), max) / (max - min) * 100}%`
   };
 
-  static defaultProps = {
-    max: 100,
-    min: 0
-  };
-
-  render() {
-    const {
-      className,
-      value,
-      max,
-      min
-    } = this.props;
-
-    const valueStyle = {
-      width: `${Math.min(Math.max(min, value), max) / (max - min) * 100}%`
-    };
-
-    return (
-      <div className={mergeClassNames(b(), className)}>
-        <div className={b("value")()} style={valueStyle} />
-      </div>
-    );
-  }
+  return (
+    <div className={mergeClassNames(b(), className)}>
+      <div className={b("value")()} style={valueStyle} />
+    </div>
+  );
 }
+
+ProgressBar.defaultProps = {
+  max: 100,
+  min: 0
+};
